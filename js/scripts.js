5ac15b1c8144d7135924ff3c39ad0dacde811753
Gabe <gabefinch@gmail.com>
2015-02-20 14:07:53 -0800

    Add jquery total pieces, total cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
-var Pizza = {size: 12,
+var Pizza = {size: 'medium',
             toppings: undefined,
-            delivery: false,
-            customer: undefined};
+            style: 'sicilian',
+            delivery: false,};
 
 Pizza.sliceCount = function() {
-  if (this.size === 8) {
+  if (this.style === 'sicilian'){
+    if (this.size === 8) {
     return 6;
-  } else if (this.size === 12) {
+    } else if (this.size === 12) {
     return 10;
-  } else if (this.size === 22) {
+    } else if (this.size === 22) {
     return 18;
+    }
+  } else if (this.style === 'neopolitan') {
+    if (this.size === 8) {
+    return 4;
+    } else if (this.size === 12) {
+    return 9;
+    } else if (this.size === 22) {
+    return 16;
+    }
   }
 };
 Pizza.piePrice = function() {
   var total, toppingRate;
-  if (this.size === 8) {
+  if ((this.size === 8) && (this.style === 'sicilian')) {
     total = 9, toppingRate = 1;
-  } else if (this.size === 12) {
+  } else if ((this.size === 12) && (this.style === 'sicilian')) {
     total = 14, toppingRate = 2;
-  } else if (this.size === 22) {
+  } else if ((this.size === 22) && (this.style === 'sicilian')) {
     total = 20, toppingRate = 3;
+  } else if((this.size === 8) && (this.style === 'neopolitan')) {
+    total = 12, toppingRate = 1;
+  } else if ((this.size === 12) && (this.style === 'neopolitan')) {
+    total = 19, toppingRate = 2;
+  } else if ((this.size === 22) && (this.style === 'neopolitan')) {
+    total = 27, toppingRate = 3;
   }
   total += toppingRate * (this.toppings.length - 2);
   return total;
@@ -32,30 +48,35 @@ $(function(){
     event.preventDefault();
     newPizza = Object.create(Pizza);
     newPizza.size = parseInt($('#selectSize').val());
+    newPizza.style = $('#selectStyle').val()
     newPizza.toppings = [];
-    var pieText = '<h4>' + newPizza.size + '" pizza </h4>'
+    var pieText = '<h4>' + newPizza.size + '" ' + newPizza.style + ' pizza </h4>'
     if ($('#tomato-sauce').prop('checked')) {
       newPizza.toppings.push('tomato sauce');
-      pieText = pieText + '<h6>tomato sauce</h6>';
+      pieText = pieText + '<h6>-tomato sauce</h6>';
       }
     if ($('#cheese').prop('checked')) {
       newPizza.toppings.push('cheese');
-      pieText = pieText + '<h6>mozarella</h6>';
+      pieText = pieText + '<h6>-mozarella</h6>';
       }
     if ($('#pepperoni').prop('checked')) {
       newPizza.toppings.push('pepperoni');
-      pieText = pieText + '<h6>pepperoni</h6>';
+      pieText = pieText + '<h6>-pepperoni</h6>';
       }
     if ($('#anchovies').prop('checked')) {
       newPizza.toppings.push('anchovies');
-      pieText = pieText + '<h6>anchovies</h6>';
+      pieText = pieText + '<h6>-anchovies</h6>';
       }
     if ($('#arugala').prop('checked')) {
       newPizza.toppings.push('arugala');
-      pieText = pieText + '<h6>arugala</h6>';
+      pieText = pieText + '<h6>-arugala</h6>';
       }
     $('#no-pie').remove();
     $('#order').append(pieText);
+    var pieces = parseInt($('#total-pieces').text());
+    $('#total-pieces').text(pieces + newPizza.sliceCount());
+    var cost = parseFloat($('#total-cost').text());
+    $('#total-cost').text((cost + newPizza.piePrice()).toFixed(2));
 
   });
 });
--- specs/specs.js
+++ specs/specs.js
@@ -1,43 +1,88 @@
 describe('Pizza', function() {
   describe('sliceCount', function(){
-    it('returns 6 for an 8" pie', function(){
+    it('returns 6 for an 8" sicilian pie', function(){
       var smallPie = Object.create(Pizza);
       smallPie.size = 8;
       expect(smallPie.sliceCount()).to.equal(6);
     });
-    it('returns 10 for an 12" pie', function(){
+    it('returns 10 for an 12" sicilian pie', function(){
       var medPie = Object.create(Pizza);
       medPie.size = 12;
       expect(medPie.sliceCount()).to.equal(10);
     });
-    it('returns 16 for an 22" pie', function(){
+    it('returns 18 for an 22" sicilian pie', function(){
       var largePie = Object.create(Pizza);
       largePie.size = 22;
       expect(largePie.sliceCount()).to.equal(18);
     });
+
+    it('returns 4 for an 8" neopolitan pie', function(){
+      var smallPie = Object.create(Pizza);
+      smallPie.size = 8;
+      smallPie.style = 'neopolitan';
+      expect(smallPie.sliceCount()).to.equal(4);
+    });
+    it('returns 9 for an 12" neopolitan pie', function(){
+      var medPie = Object.create(Pizza);
+      medPie.size = 12;
+      medPie.style = 'neopolitan';
+      expect(medPie.sliceCount()).to.equal(9);
+    });
+    it('returns 16 for an 22" neopolitan pie', function(){
+      var largePie = Object.create(Pizza);
+      largePie.size = 22;
+      largePie.style = 'neopolitan';
+      expect(largePie.sliceCount()).to.equal(16);
+    });
   });
   describe('piePrice', function(){
-    it('returns $9 for an 8" plain cheese pie', function(){
+    it('returns $9 for an 8" plain cheese sicilian pie', function(){
       var smallPie = Object.create(Pizza);
       smallPie.size = 8;
       smallPie.toppings = ['tomato sauce', 'cheese'];
       expect(smallPie.piePrice()).to.equal(9);
     });
-    it('returns $16 for an 12" pepperoni pie', function(){
+    it('returns $16 for an 12" pepperoni sicilian pie', function(){
       var medPie = Object.create(Pizza);
       medPie.size = 12;
       medPie.toppings = ['tomato sauce', 'cheese'];
       medPie.toppings.push('pepperoni');
       expect(medPie.piePrice()).to.equal(16);
     });
-    it('returns $29 for an 22" pepperoni/olives/sardines pie', function(){
+    it('returns $29 for an 22" pepperoni/anchovy/arugala sicilian pie', function(){
       var largePie = Object.create(Pizza);
       largePie.size = 22;
       largePie.toppings = ['tomato sauce', 'cheese'];
       largePie.toppings.push('pepperoni');
-      largePie.toppings.push('sardines');
-      largePie.toppings.push('olives');
+      largePie.toppings.push('anchovies');
+      largePie.toppings.push('arugala');
       expect(largePie.piePrice()).to.equal(29);
+    });
+
+    it('returns $12 for an 8" plain cheese neopolitan pie', function(){
+      var smallPie = Object.create(Pizza);
+      smallPie.size = 8;
+      smallPie.style = 'neopolitan';
+      smallPie.toppings = ['tomato sauce', 'cheese'];
+      expect(smallPie.piePrice()).to.equal(12);
+    });
+    it('returns $21 for an 12" pepperoni neopolitan pie', function(){
+      var medPie = Object.create(Pizza);
+      medPie.size = 12;
+      medPie.style = 'neopolitan';
+      medPie.toppings = ['tomato sauce', 'cheese'];
+      medPie.toppings.push('pepperoni');
+      expect(medPie.piePrice()).to.equal(21);
+    });
+    it('returns $36 for an 22" pepperoni/anchovy/arugala neopolitan pie', function(){
+      var largePie = Object.create(Pizza);
+      largePie.size = 22;
+      largePie.style = 'neopolitan';
+      largePie.toppings = ['tomato sauce', 'cheese'];
+      largePie.toppings.push('pepperoni');
+      largePie.toppings.push('anchovies');
+      largePie.toppings.push('arugala');
+      expect(largePie.piePrice()).to.equal(36);
     });
   });
 });
